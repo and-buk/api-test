a1d8008d6989b552bbd7e613f225a76aad4a4553
and-buk <and.bukalov@gmail.com>
2021-10-05 13:42:19 +0300

    Add change item info test

--- tests/store_item/test_change_item.py
+++ tests/store_item/test_change_item.py
@@ -3,18 +3,18 @@ from fixtures.store_item.model import Item
 
 
 class TestStoreItem:
-    def test_change_item(self, app, store):
+    def test_change_item(self, app, item):
         """
         Steps.
-            1. Try to login user with valid data
+            1. Register new user
             2. Check that status code is 200
             3. Check response
         """
-        data = Item.random(store.store_uuid)
+        data = Item.random(item.store_uuid, name=item.item)
         res = app.operations_with_store_item.change_item(
-            name_item=data.name,
+            name_item=item.item,
             data=data,
-            header=store.header,
+            header=item.header,
             type_response=StoreResponse,
         )
-        assert res.status_code == 201
+        assert res.status_code == 200
